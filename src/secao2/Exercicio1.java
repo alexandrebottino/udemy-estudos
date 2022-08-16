4902862736e037fa03af3832f24e4e3b6e4a42d2
Alexandre Bottino <alexandre.bottino@gmail.com>
2022-08-16 17:40:15 -0300

    Aula do dia 16/08/2022

--- src/secao2/Exercicio1.java
+++ src/secao2/Exercicio1.java
@@ -1,3 +1,5 @@
+package secao2;
+
 import java.util.Scanner;
 
 public class Exercicio1 {
---
+++ src/secao3/If.java
@@ -0,0 +1,27 @@
+package secao3;
+
+import java.util.Scanner;
+
+public class If {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Informe a média do aluno: ");
+        double media =  scanner.nextDouble();
+
+        if(media <= 10 && media >= 7) {
+            System.out.println("Aprovado");
+            System.out.println("Parabens");
+        }
+
+        if (media < 7 && media >= 4.5){
+            System.out.println("Recuperação");
+        }
+
+        if (media < 4.5 && media >= 0){
+            System.out.println("Reprovado");
+        }
+
+        scanner.close();
+    }
+}
