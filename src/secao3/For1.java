b29826827fe2d520f751901339028a0c823126a6
alexandrebottino <alexandre.bottino@gmail.com>
2022-08-16 22:33:19 -0300

    Estudos norturno, dia 16/08

---
+++ src/secao3/For1.java
@@ -0,0 +1,14 @@
+package secao3;
+
+public class For1 {
+    public static void main(String[] args) {
+        //int contador = 2;
+        //while (contador <= 10){
+        //    System.out.println(contador);
+        //    contador+= 2;
+
+        for (int contador = 0; contador <= 20; contador++) {
+            System.out.println(contador);
+        }
+    }
+}
---
+++ src/secao3/WhileDeterminado.java
@@ -0,0 +1,11 @@
+package secao3;
+
+public class WhileDeterminado {
+    public static void main(String[] args) {
+        int contador = 2;
+        while (contador <= 10){
+            System.out.println(contador);
+            contador+= 2;
+        }
+    }
+}
